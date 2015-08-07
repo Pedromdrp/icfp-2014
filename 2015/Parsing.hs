37fcfb260ba7bf50d40fce03473eb2605082ba3d
Pedro da Rocha Pinto <pmd09@doc.ic.ac.uk>
2015-08-07 14:54:47 +0100

    Added JSON parser.

---
+++ 2015/Parsing.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Datastructures
+import Data.Aeson
+import Data.Text
+import Control.Applicative
+import Control.Monad
+import qualified Data.ByteString.Lazy as B
+
+--instance FromJSON Cell
+
+instance FromJSON Cell where
+ parseJSON (Object v) =
+    Cell <$> v .: "x"
+         <*> v .: "y"
+ parseJSON _ = mzero
+
+instance FromJSON Unit where
+ parseJSON (Object v) =
+    Unit <$> v .: "members"
+         <*> v .: "pivot"
+ parseJSON _ = mzero
+
+instance FromJSON Configuration where
+ parseJSON (Object v) =
+    Configuration <$> v .: "id"
+                  <*> v .: "units"
+                  <*> v .: "width"
+                  <*> v .: "height"
+                  <*> v .: "filled"
+                  <*> v .: "sourceLength"
+                  <*> v .: "sourceSeeds"
+ parseJSON _ = mzero
+
+jsonFile :: FilePath
+jsonFile = "qualifiers/problem_0.json"
+
+getJSON :: IO B.ByteString
+getJSON = B.readFile jsonFile
+
+main :: IO ()
+main = do
+  d <- (eitherDecode <$> getJSON) :: IO (Either String Configuration)
+  case d of
+    Left err -> putStrLn err
+    Right ps -> print ps
