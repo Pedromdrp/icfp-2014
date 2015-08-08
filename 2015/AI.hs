d4e7fad749b8ef5208dd9ade535514a59ede5694
Pedro da Rocha Pinto <pmd09@doc.ic.ac.uk>
2015-08-08 09:48:08 +0100

    Fixed function.

--- 2015/AI.hs
+++ 2015/AI.hs
@@ -8,8 +8,8 @@ equalUnit :: Unit -> Unit -> Bool
 equalUnit (Unit c1 p1) (Unit c2 p2) = (p1 == p2) && (sort c1 == sort c2)
 
 -- ^Get list of rotation angles that yield an equivalent unit
-getSymmetryAngles :: Unit -> [Int]
-getSymmetryAngles u = getSymmetryAnglesAux u [60,120..300]
-  where getSymmetryAnglesAux un [] = []
-        getSymmetryAnglesAux un (x:xs) = (if (equalUnit u un2) then [x] else []) ++ (getSymmetryAnglesAux un2 xs)
+getSymmetryAngles :: Unit -> Int
+getSymmetryAngles u = getSymmetryAnglesAux u [1, 2, 3]
+  where getSymmetryAnglesAux un [] = 6
+        getSymmetryAnglesAux un (x:xs) = if (equalUnit u un2) then x else (getSymmetryAnglesAux un2 xs)
           where un2 = rotateACWUnit un
