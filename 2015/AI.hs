a9fe9a505fbf774cbe178aa48f060555645a07b2
Pedro da Rocha Pinto <pmd09@doc.ic.ac.uk>
2015-08-08 09:41:33 +0100

    Added symmetry functions.

---
+++ 2015/AI.hs
@@ -0,0 +1,15 @@
+module AI where
+import Datastructures
+import Moves
+import Data.List
+
+-- ^Tests if two units with the same pivot are equivalent
+equalUnit :: Unit -> Unit -> Bool
+equalUnit (Unit c1 p1) (Unit c2 p2) = (p1 == p2) && (sort c1 == sort c2)
+
+-- ^Get list of rotation angles that yield an equivalent unit
+getSymmetryAngles :: Unit -> [Int]
+getSymmetryAngles u = getSymmetryAnglesAux u [60,120..300]
+  where getSymmetryAnglesAux un [] = []
+        getSymmetryAnglesAux un (x:xs) = (if (equalUnit u un2) then [x] else []) ++ (getSymmetryAnglesAux un2 xs)
+          where un2 = rotateACWUnit un
